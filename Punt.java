ab0af3d73c88c7a18f183f75c0bc2d021c8c01fa
alexvp21 <cf17alexander.vargas@iesjoandaustria.org>
2019-05-04 13:59:57 +0200

    Ejercicio 2

--- Punt.java
+++ Punt.java
@@ -3,6 +3,15 @@ package punts;
 public class Punt {
 	private int x, y; // coordenades del punt
 
+	public Punt(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Punt() {
+		this(0, 0);
+	}
+
 	public int getX() {
 		return x;
 	}
